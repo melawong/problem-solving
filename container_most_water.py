566763310c4f7df9f967a78d50f1423d35b9d08f
Melanie Wong <wong.mela@gmail.com>
2022-07-25 18:43:53 -0700

    invert binary tree, container_most_water

---
+++ container_most_water.py
@@ -0,0 +1,18 @@
+def maxArea(height):
+    max_area = 0
+    start = 0
+    end = len(height) - 1
+
+    while start < end:
+        length = min(height[start], height[end])
+        width = end - start
+        area = length * width
+        print(area, height[start], height[end], length, width)
+        max_area = max(max_area, area)
+
+        if(height[start] < height[end]):
+            start += 1
+        else:
+            end -= 1
+
+    return max_area
